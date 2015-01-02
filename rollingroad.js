44ac9d8bad6380e9387adfc976c2ced5f1277cde
garrypas <garry.passarella@gmail.com>
2015-01-02 23:50:09 +0000

    main js file

---
+++ rollingroad.js
@@ -0,0 +1,69 @@
+var RollingRoad = (function() {
+    var _testCases = [],
+        _iterations = 10000,
+        _samples = 5,
+        _results;
+
+    var rollingRoad = function() {
+        var args = [0, _testCases.length].concat( Array.prototype.slice.call(arguments));
+        Array.prototype.splice.apply(_testCases, args);
+    };
+
+    var now = function() {
+        return new Date().getMilliseconds();
+    };
+    
+    var _doIteration = function() {
+        var ops = new Array(_testCases.length),
+            t,
+            time,
+            i;
+        
+        for(t = 0; t < _testCases.length; t++) {
+            time = now();
+            for(i = 0; i < _iterations; i++) {
+                _testCases[t].test();
+            }
+            ops[t] = (typeof ops[t] == 'undefined' ? 0 : ops[t]) + now() - time;
+        }
+        return ops;
+    };
+    
+    var _doSample = function() {
+        var samples = new Array(_samples),
+            s;
+        for(s = 0; s < _samples; s++) {
+            samples[s] = _doIteration();
+        }
+        return samples;
+    };
+    
+    rollingRoad.prototype.run = function() {
+        var samples = _doSample(),
+            results = new Array(_testCases.length),
+            t,
+            s;
+        for(t = 0; t < _testCases.length; t++) {
+            results[t] = { name : _testCases[t].name,
+                          samples : 0, duration : 0 };
+            for(s = 0; s < samples.length; s++) {
+                results[t].samples ++;
+                results[t].duration += samples[s][t];
+            }
+        }
+        _results = results;
+        return this;
+    };
+    
+    rollingRoad.prototype.done = function(callback) {
+        callback(_results);
+    };
+    
+    rollingRoad.prototype.withOptions = function(options) { 
+        _samples = options.samples || _samples;
+        _iterations = options.iterations || _iterations;
+        return this;
+    };
+    
+    return rollingRoad;
+}());
